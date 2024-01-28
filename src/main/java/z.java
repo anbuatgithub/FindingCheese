d152ab6aad1ea255140d9ea8b233461e4bf7e11a
anbalagan marimuthu <anbalagan.marimuthu@gmail.com>
2024-01-28 23:11:11 +0000

    '2'

---
+++ src/main/java/z.java
@@ -0,0 +1,26 @@
+public class z {
+
+    public static void main(String[] args) {
+        b bj= new c();
+        bj.m1();
+    }
+}
+
+ class b {
+
+    void m1() throws  ArrayIndexOutOfBoundsException{
+
+    }
+}
+
+ class c extends b {
+
+    void m1() throws  IndexOutOfBoundsException{
+
+    }
+     void m2(){
+
+     }
+
+}
+
